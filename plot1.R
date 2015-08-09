965777b65c3dd30bb3e526111cb25bf23916f959
pikerg <pike_richard1@columbusstate.edu>
2015-08-09 18:30:48 -0400

    Add project files to repository
    Initial load of project files

---
+++ plot1.R
@@ -0,0 +1,40 @@
+################################################################################################
+## Loading the data set
+##  *The dataset has 2,075,259 rows and 9 columns. First calculate a rough estimate of how much
+##    memory the dataset will require in memory before reading into R. Make sure your computer
+##    has enough memory (most modern computers should be fine).
+##  *We will only be using data from the dates 2007-02-01 and 2007-02-02. One alternative is to
+##    read the data from just those dates rather than reading in the entire dataset and
+##    subsetting to those dates.
+##  *You may find it useful to convert the Date and Time variables to Date/Time classes in R
+##    using the strptime() and as.Date() functions.
+##  *Note that in this dataset missing values are coded as ?.
+################################################################################################
+##
+## install required pakage - install.packages('sqldf')
+## Load required package - library('sqldf')
+##
+################################################################################################
+
+## Set file path
+file <- "./household_power_consumption.txt"
+
+## SQL statement for filtering on date. 
+mySql <- "select * from file where Date in ('1/2/2007', '2/2/2007')"
+
+## Read data set into environment. Filter using SQL statement setting the delimeter as ";"
+##  and setting missing values to "na"
+dt <- read.csv2.sql(file, mySql, header=T, sep=';',na.strings='?', dec='.', stringsAsFactors=F, quote='\"')
+
+## convert globalActivePower to numbers
+dt$globalActivePower <- as.numeric(dt$Global_active_power)
+
+## Save the file
+png(file="plot1.png", width=480, height=480)
+
+## Plot Histogram
+hist(dt$Global_active_power, main='Global Active Power', xlab='Global Active Power (kilowatts)',
+     ylab='Frequency', col='Red')
+
+## required to close connection
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,52 @@
+################################################################################################
+## Loading the data set
+##  *The dataset has 2,075,259 rows and 9 columns. First calculate a rough estimate of how much
+##    memory the dataset will require in memory before reading into R. Make sure your computer
+##    has enough memory (most modern computers should be fine).
+##  *We will only be using data from the dates 2007-02-01 and 2007-02-02. One alternative is to
+##    read the data from just those dates rather than reading in the entire dataset and
+##    subsetting to those dates.
+##  *You may find it useful to convert the Date and Time variables to Date/Time classes in R
+##    using the strptime() and as.Date() functions.
+##  *Note that in this dataset missing values are coded as ?.
+################################################################################################
+##
+## install required pakage - install.packages('sqldf')
+## Load required package - library('sqldf')
+##
+################################################################################################
+
+## Set file path
+file <- "./household_power_consumption.txt"
+
+## SQL statement for filtering on date. 
+mySql <- "select * from file where Date in ('1/2/2007', '2/2/2007')"
+
+## Read data set into environment. Filter using SQL statement setting the delimeter as ";"
+##  and setting missing values to "na"
+dt <- read.csv2.sql(file, mySql, header=T, sep=';',na.strings='?', dec='.', stringsAsFactors=F, quote='\"')
+
+## Add datetime column to data for ISO standard dates and tiimes formating
+dt$datetime <- strptime(paste(dt$Date, dt$Time, sep=" "), "%d/%m/%Y %T")
+
+## Format strings to numbers columns for plotting
+dt$globalActivePower <- as.numeric(dt$Global_active_power)
+dt$subMetering1 <- as.numeric(dt$Sub_metering_1)
+dt$subMetering2 <- as.numeric(dt$Sub_metering_2)
+dt$subMetering3 <- as.numeric(dt$Sub_metering_3)
+
+## Create vectors for legend groups and colors
+legGroup <- c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3')
+legColor <- c('black', 'red', 'blue')
+
+## Save the file
+png(filename="plot3.png", width=480, height=480)
+
+## Plot 2 day graph
+plot(dt$datetime, dt$subMetering1, type='l', xlab='', ylab='Energy sub metering')
+lines(dt$datetime, dt$subMetering2, type='l', col='red')
+lines(dt$datetime, dt$subMetering3, type='l', col='blue')
+legend('topright', legend=legGroup, lty=1, lwd=2, col=legColor)
+
+## required to close connection
+dev.off()
